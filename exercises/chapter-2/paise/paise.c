5e4b3bfa722ba5057d1f2726ef664e60274086a8
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-07-01 15:44:26 +0200

    .

--- exercises/chapter-2/paise/paise.c
+++ exercises/chapter-2/paise/paise.c
@@ -10,7 +10,7 @@ int main(void)
     // program that reads a decimal form input and output it into integer
     // ex: 15.95 => 1595
 
-    double number = 14222.19861;
+    double number = 19.991131; // doubles have a limit something like 1.19861 would give you a bad error
 
     if (is_decimal(number))
     {
@@ -46,12 +46,31 @@ double is_decimal(double num)
 
 int convert_to_int(double num)
 {
+    // this function may produce incorrect result
+    // because of how double and floating-point numbers work in C
+    // — they often cannot represent decimal values exactly.
+    // use it with caution.
+    // Numbers like 1231.1986 can't be stored precisely in binary.
+
+    // So num might never become an exact integer, even after many multiplications
+    // the condition (int)num != num might never be false, resulting in an infinite loop.
+    // which we tried to force escape by limiting loops to 5
+    // so this works well only up to 4 or 5 decimal points -- beyond that - the other decimals are ignored.
+
+    int count = 0;
+
     while ((int)num != num)
     {
+        if (count > 4)
+        {
+            break;
+        }
         num *= pow(10, 1);
+        count++;
     }
     return num;
 }
+
 
 // int convert_to_int(doublenum)
 // {
