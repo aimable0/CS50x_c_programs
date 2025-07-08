662dcaab361768fafb9c893ff965b5519f3ab82b
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-07-08 18:50:28 +0200

    .

--- readability/readability.c
+++ readability/readability.c
@@ -64,10 +64,10 @@ int main(void)
     }
     else if (index > 16)
     {
-        printf("Grade: 16+\n");
+        printf("Grade 16+\n");
     }
     else{
-        printf("Grade: %i\n", index);
+        printf("Grade %i\n", index);
     }
     return 0;
 }
