59b4203cb1597ff709a2f089939b7c89a09b3b0e
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-07-14 11:29:16 +0200

    cipher

---
+++ caesar/caesar.c
@@ -0,0 +1,58 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+string encrypt(string text, int key);
+
+int main(int argc, string argv[])
+{
+    // check if the usage of the program is correct.
+    if (argc < 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+
+    // look for a function that can modify a string (CLA) into an int
+    int key;
+
+    if (atoi(argv[1]) >= 0)
+    {
+        key = atoi(argv[1]);
+    }
+    else
+    {
+        printf("Key has to be a non-negative integer\n");
+    }
+
+    // get plain text from user.
+    string text = get_string("plain text:  ");
+
+    // encrypt text.
+    // string cipher_text = encrypt(text, key);
+
+    printf("cipher text: %s\n",encrypt(text, key));
+
+    return 0;
+}
+
+string encrypt(string text, int key)
+{
+    // string encrypted_text;
+    string text_cpy = text;
+    int cypher_key = key % 26;
+
+    for (int i = 0, len = strlen(text_cpy); i < len; i++)
+    {
+        // the isalpha sets a condition to ensure only alphabets are rotated.
+        if (isalpha(text_cpy[i]))
+        {
+            text_cpy[i] = ((text_cpy[i] + cypher_key) > 122) ? text_cpy[i] + cypher_key - 26 : text_cpy[i] + cypher_key;
+        }
+    }
+
+    return text_cpy;
+}
---
+++ caesar/ternary_ops.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int age = (3 > 4) ? 5 : 10;
+
+    printf("%i\n", age);
+}
